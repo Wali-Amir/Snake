63074e1b0dfccddfd5285a8b7fb133546b127d27
Wali Bin Amir <waliamir.339@gmail.com>
2025-12-27 12:43:40 +0500

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,111 @@
+import random
+
+#Constants:
+UP = (1, 0)
+DOWN = (-1, 0)
+LEFT = (0, -1)
+RIGHT = (0, 1)
+
+#Classes:
+class Snake:
+    #PUBLIC direction : TUPLE
+    #PUBLIC body : TUPLE
+
+    def __init__(self,initial_body, initial_direction):
+        self.body = initial_body
+        self.direction = initial_direction
+    
+    def take_step(self, position):
+        self.body = self.body[1:] + [position]
+
+    def set_direction(self,direction):
+        self.direction = direction
+
+    def head(self):
+        return self.body[-1]
+    
+    def make_move(self,move,invincible,grid):
+        new_row = self.head()[0]+move[0]
+        new_col = self.head()[1]+move[1]
+        if invincible:
+            if new_row>=grid[0]:
+                new_row = 0
+            elif new_row<0:
+                new_row = grid[0]-1
+            if new_col>=grid[1]:
+                new_col = 0
+            elif new_col<0:
+                new_col = grid[1]-1
+
+        new_head = (new_row,new_col)
+        self.take_step(new_head)
+
+    def grow(self,invincible,grid):
+        new_row = self.head()[0]+self.direction[0]
+        new_col = self.head()[1]+self.direction[1]
+        if invincible:
+            if new_row>=grid[0]:
+                new_row = 0
+            elif new_row<0:
+                new_row = grid[0]-1
+            if new_col>=grid[1]:
+                new_col = 0
+            elif new_col<0:
+                new_col = grid[1]-1
+
+        new_head = (new_row,new_col)
+        self.body = self.body + [new_head]
+
+class Apple:
+    #PUBLIC position : TUPLE
+
+    def __init__(self, position):
+        self.position = position
+
+class Game:
+    #PUBLIC height, width: INTEGER
+    #PUBLIC snake: Snake
+
+    def __init__(self, height, width, init_body, init_direction):
+        self.height = height
+        self.width = width
+        self.snake = Snake(init_body, init_direction)
+        self.apple = Apple((random.randint(0,height-1),random.randint(0,width-1)))
+
+    def board_matrix(self):
+        matrix = [[None for _ in range(self.width)] for _ in range(self.height)]
+        return matrix
+
+    def render(self):
+        matrix = self.board_matrix()
+        width = self.width
+        height = self.height
+
+        print('+','-'*width,'+',sep="") #top border
+
+        for row in range(height): #row loop
+            print('|',end="")
+            for col in range(width): #col loop
+                printed = False
+                if (height-1-row,col) == self.snake.head(): #if this is head position print X
+                    print('X',end='')
+                    printed=True
+                else:
+                    if (height-1-row,col) == self.apple.position and printed==False:
+                        print('*',end="")
+                        printed = True
+                    for part in self.snake.body: #check if body position and print O
+                        if(height-1-row,col) == part and printed==False:
+                            print('O',end='')
+                            printed = True
+                if printed==False: #if neither print blank space
+                    print(' ',end='')
+            print('|') 
+
+        print('+','-'*width,'+',sep="") #bottom border
+
+    def grow_snake(self,invincible):
+        self.snake.grow(invincible,(self.height,self.width))
+        self.apple.position = (random.randint(0,self.height-1),random.randint(0,self.width-1))
+        while self.apple.position == self.snake.head():
+            self.apple.position = (random.randint(0,self.height-1),random.randint(0,self.width-1))
---
+++ game.py
@@ -0,0 +1,41 @@
+import classes
+import random
+
+grid_sizes = {1:(15,17),2:(9,10),3:(21,24)}
+incompatible = {classes.UP:classes.DOWN,classes.LEFT:classes.RIGHT,classes.DOWN:classes.UP,classes.RIGHT:classes.LEFT}
+
+grid = grid_sizes[int(input("Enter which grid size you want to play with:\n1. Standard Size (15x17)\n2. Small Size (9x10)\n3. Large Size (21x24)\nEnter your choice: "))]
+game_end = False
+invincible = int(input("Do you want wall colission? (1 for Yes, 0 for No) ")) == 0
+start_col = int(grid[0]/2)
+init_body = [(2,start_col),(3,start_col),(4,start_col),(5,start_col),(6,start_col)]
+ourGame = classes.Game(grid[0],grid[1],init_body,classes.UP)
+score = 0
+
+while not game_end:
+    ourGame.render()
+    moves = {"W":classes.UP,"A":classes.LEFT,"S":classes.DOWN,"D":classes.RIGHT,'':ourGame.snake.direction}
+    move = moves[input("W for UP, A for LEFT, S for DOWN, D for RIGHT, Nothing to keep going straight: ")]
+    while (move == incompatible[ourGame.snake.direction]):
+        print("That is an incompatible move")
+        move = moves[input("W for UP, A for LEFT, S for DOWN, D for RIGHT: ")]
+
+    ourGame.snake.set_direction(move)
+    ourGame.snake.make_move(move,invincible,grid)
+
+    if ourGame.snake.head()==ourGame.apple.position:
+        ourGame.grow_snake(invincible)
+        score += 1
+
+    if (not invincible):
+        if ourGame.snake.head()[0]<0 or ourGame.snake.head()[0]>=grid[0] or ourGame.snake.head()[1]<0 or ourGame.snake.head()[1]>=grid[1]:
+            game_end=True
+            print(f"The snake has crashed into a wall. The game has ended.\nYou scored {score} points")
+    
+    for part in ourGame.snake.body[0:-1]:
+        if ourGame.snake.head()==part:
+            game_end=True
+            print(f"The snake has crashed into itself. The game has ended.\nYou scored {score} points")
+
+
+
